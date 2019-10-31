525a986361f00a9d54d49ec5b6b3225e9521cf55
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-31 21:14:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
+    return fetch("https://anapioficeandfire.com/api/books")
 }
 
 function renderBooks(json) {
+
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
+    console.log(book.name);
   })
 }
 
