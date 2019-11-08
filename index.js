979b9643902bd7a6d539a9eedaa6ba480e2bbc68
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-11-08 22:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function fetchBooks() {
+<<<<<<< HEAD
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
     .then(json => renderBooks(json));
@@ -7,11 +8,26 @@ function fetchBooks() {
 
 function renderBooks(json) {
 
+=======
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(resp => resp.json())
+      .then(json => renderBooks(json));
+      return fetch("https://anapioficeandfire.com/api/books");
+      
+}
+
+function renderBooks(json) {
+  
+>>>>>>> 98e0f56d1126e2116c859cfc264a19a8d907d107
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
+<<<<<<< HEAD
     main.appendChild(h2)
+=======
+    main.appendChild(h2) 
+>>>>>>> 98e0f56d1126e2116c859cfc264a19a8d907d107
     console.log(book.name);
   })
 }
